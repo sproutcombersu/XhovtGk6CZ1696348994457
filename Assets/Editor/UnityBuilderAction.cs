a8095f0eaf3615fbd8e4638549f61ba7de1c6a74
EasyApk <easyapk@icloud.com>
2023-10-06 13:46:05 +0000

    update

--- Assets/Editor/UnityBuilderAction.cs
+++ Assets/Editor/UnityBuilderAction.cs
@@ -12,13 +12,13 @@ namespace UnityBuilderAction
     {
         private static readonly string Eol = Environment.NewLine;
         private static string APP_NAME = "Casino Royal - Slot Cash";
-        private static string COMPANY_NAME = "169658905485688428414";
+        private static string COMPANY_NAME = "1696599952694352436869";
         private static string BUILD_VERSION_NAME = "1.0.0";
         private static string PACKAGENAME = "com.bayangkanharimu.penuhwarna";
         private static string PATH_KEYSTORE = "data.keystore";
-        private static string KEYSTORE_PASS = "bersabardarisemuanya";
-        private static string KEY_ALIAS_NAME = "bersabardarisemuanya";
-        private static string KEY_ALIAS_PASS = "bersabardarisemuanya";
+        private static string KEYSTORE_PASS = "akandatanghariyangindah";
+        private static string KEY_ALIAS_NAME = "akandatanghariyangindah";
+        private static string KEY_ALIAS_PASS = "akandatanghariyangindah";
 
         private static readonly string[] Secrets =
             {"androidKeystorePass", "androidKeyaliasName", "androidKeyaliasPass"};
